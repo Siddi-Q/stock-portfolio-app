b8aa844d1af9471925a78ef852b3a165cff6256a
Saddique Shafi <saddiqueashafi@gmail.com>
2020-06-03 16:01:46 -0400

    Updated routers/users.js

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -22,59 +22,73 @@ router.post('/signin', async (req, res) => {
         const authToken = await user.generateAuthToken();
         res.send({ user, authToken });
     } catch (error) {
-        res.status(400).send(error);
+        res.status(400).send({message: "Incorrect username or password!"});
+    }
+});
+
+router.post('/verify', auth, (req, res) => {
+    try {
+        res.send(true);
+    } catch (error) {
+        res.status(500).send({message: "Server error!"});
     }
 });
 
 // Todo: cleanup
 router.post('/buy', auth, async (req, res) => {
-    const stock  = req.body;
-    const api_url = `https://sandbox.iexapis.com/stable/stock/${stock.tickerSymbol}/quote?token=${process.env.iexToken}`;
+    let {ticker, quantity} = req.body;
+    ticker = ticker.toUpperCase();
+    quantity = Number(quantity);
+
+    const api_url = `https://sandbox.iexapis.com/stable/stock/${ticker}/quote?token=${process.env.iexToken}`;
 
     try {
-        if(!Number.isInteger(stock.quantity) || stock.quantity < 0 ){
+        if(!Number.isInteger(quantity) || quantity < 0 ) {
             throw new Error( "Quantity is not a whole number!");
         }
 
-        const user = await User.findById(req.user.id);
+        let { balance, portfolio, transactions } = req.user;
         const response = await axios.get(api_url);
-        const latestPrice = response.data.latestPrice;
-        const totalPrice = latestPrice * stock.quantity;
 
-        if (totalPrice > user.balance) {
+        // Get the latest price of the stock.
+        let latestPrice;
+        if(response.data.latestPrice == null) {
+            latestPrice = response.data.previousClose;
+        } 
+        else {
+            latestPrice = response.data.latestPrice;
+        }
+        const totalPrice = latestPrice * quantity;
+
+        // Check if the user has enough cash to make the purchase.
+        if (totalPrice > balance) {
             throw new Error("Purchase amount is greater than your balance!");
         }
         
         // Update balance
-        user.balance = user.balance - totalPrice;
+        balance = balance - totalPrice;
 
         // Update transactions
-        user.transactions = user.transactions.concat({ 
-            tickerSymbol: stock.tickerSymbol.toUpperCase(), 
-            quantity: stock.quantity, 
-            totalPrice: totalPrice});
+        transactions = transactions.concat({ ticker, quantity, price: latestPrice });
         
         // Update portfolio
         let updated = false;
-        const lenPortfolio = user.portfolio.length;
-        for(let i = 0; i < lenPortfolio; i++){
-            if(user.portfolio[i].tickerSymbol.toUpperCase() === stock.tickerSymbol.toUpperCase()){
-                user.portfolio[i].quantity += stock.quantity;
+        const lenPortfolio = portfolio.length;
+        for(let i = 0; i < lenPortfolio; i++) {
+            if(portfolio[i].ticker.toUpperCase() === ticker) {
+                portfolio[i].quantity += quantity;
                 updated = true;
                 break;
             }
         }
 
         if(!updated){
-            user.portfolio = user.portfolio.concat({tickerSymbol: stock.tickerSymbol.toUpperCase(), 
-                                                    quantity: stock.quantity});
+            portfolio = portfolio.concat({ ticker, quantity });
         }
         
         // Update database with updated values
-        await User.findByIdAndUpdate(req.user.id, { transactions: user.transactions,
-                                                    balance: user.balance,
-                                                    portfolio: user.portfolio});
-        res.status(201).send(stock);
+        await User.findByIdAndUpdate(req.user.id, { transactions, balance, portfolio });
+        res.status(201).send("Good!"); // Todo: Update 
     } catch (error) {
         res.status(400).send(error);
     }
@@ -83,52 +97,73 @@ router.post('/buy', auth, async (req, res) => {
 // Todo: Cleanup
 router.get('/portfolio', auth, async (req, res) => {
     try {
-        const portfolio = await User.findById(req.user.id, 'portfolio');
-        let portfolioList = portfolio.portfolio;
+        const portfolioList = req.user.portfolio;
+        const balance = req.user.balance;
+        let totalPortfolioPrice = 0;
+
+        // Get list of ticker symbols in the user's portfolio
         let symbols = [];
-        for(let i = 0; i < portfolioList.length; i++){
-            symbols.push(portfolioList[i].tickerSymbol);
+        for(let i = 0; i < portfolioList.length; i++) {
+            symbols.push(portfolioList[i].ticker);
         }
-        if(symbols.length > 0){
+
+        if(symbols.length > 0) {
             let symbolsString = symbols.join();
             const api_url = `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${symbolsString}&types=quote&token=${process.env.iexToken}`;
             const response = await axios.get(api_url);
-            for(let i = 0; i < portfolioList.length; i++){
+
+            // For each stock, get the latest and open prices
+            for(let i = 0; i < portfolioList.length; i++) {
                 let latestPrice = 0;
                 let openPrice = 0;
-                if(response.data[portfolioList[i].tickerSymbol].quote.latestPrice == null){
-                    latestPrice = response.data[portfolioList[i].tickerSymbol].quote.previousClose;
+
+                if(response.data[portfolioList[i].ticker].quote.latestPrice == null) {
+                    latestPrice = response.data[portfolioList[i].ticker].quote.previousClose;
                     openPrice = latestPrice;
                 }
-                else{
-                    latestPrice = response.data[portfolioList[i].tickerSymbol].quote.latestPrice;
-                    openPrice = response.data[portfolioList[i].tickerSymbol].quote.open;
+                else {
+                    latestPrice = response.data[portfolioList[i].ticker].quote.latestPrice;
+                    openPrice = response.data[portfolioList[i].ticker].quote.open;
                 }
+
                 portfolioList[i].totalPrice = latestPrice * portfolioList[i].quantity;
-                if(latestPrice < openPrice){
+                totalPortfolioPrice += portfolioList[i].totalPrice;
+                // Set stock's current performance
+                if(latestPrice < openPrice) {
                     portfolioList[i].performance = "less";
                 }
-                else if(latestPrice == openPrice){
+                else if(latestPrice == openPrice) {
                     portfolioList[i].performance = "equal";
                 }
-                else{
+                else {
                     portfolioList[i].performance = "greater";
                 }
             }
         }
-        res.send(portfolioList);
+        res.send({portfolioList, balance, totalPortfolioPrice});
     } catch (error) {
-        console.log(error);
         res.status(500).send(error);
     }
 });
 
 router.get('/transactions', auth, async (req, res) => {
     try {
-        const transactions = await User.findById(req.user.id, 'transactions');
-        res.send(transactions.transactions);
+        const transactions = req.user.transactions;
+        res.send(transactions);
     } catch (error) {
         res.status(500).send(error);
+    }
+});
+
+router.post('/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+        res.send();
+    } catch {
+        res.status(500).send();
     }
 });
 
