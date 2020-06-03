1cfda695f0b0a21a232e97373a3d1fb68cee5e03
Saddique Shafi <saddiqueashafi@gmail.com>
2020-06-03 15:55:27 -0400

    Updated src/App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import PrivateNavbar from "./components/privateNavbar.component";
+import Signin from "./components/signin.component";
+import Register from "./components/register.component";
+import Portfolio from "./components/portfolio.component";
+import TransactionsList from "./components/transactions-list.component"
+import Logout from './components/logout.component';
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.setIsAuth = this.setIsAuth.bind(this);
+
+    this.state = {
+      isAuth: false
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await axios.post('http://localhost:5000/verify', null, {
+        headers: {Authorization: sessionStorage.token}
+      });
+      res ? this.setIsAuth(true) : this.setIsAuth(false);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  setIsAuth(value) {
+    this.setState({
+      isAuth: value
+    });
+  }
+  
+  render() {
+    return (
+      <Router>
+        {this.state.isAuth ? <PrivateNavbar /> : <Navbar />}
+        <br />
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/signin"/>}/>
+          <Route exact path="/signin" render={() => this.state.isAuth ? <Redirect to="/portfolio"/> : <Signin setIsAuth={this.setIsAuth}/>}/>
+          <Route exact path="/register" render={() => this.state.isAuth ? <Redirect to="/portfolio"/> : <Register setIsAuth={this.setIsAuth}/>}/>
+          <Route exact path="/portfolio" render={() => this.state.isAuth ? <Portfolio/> : <Redirect to="/signin"/>}/>
+          <Route exact path="/transactions" render={() => this.state.isAuth ? <TransactionsList /> : <Redirect to="/signin"/>}/>
+          <Route exact path="/logout" render={() => this.state.isAuth ? <Logout setIsAuth={this.setIsAuth}/> : <Redirect to="/signin"/>}/>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
-export default App;
