238fc5ec8e857aea7e908168aa220c935584c771
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-13 17:01:54 -0500

    Output each transaction list item as a row in a table

--- client/src/components/transactions-list-item.component.js
+++ client/src/components/transactions-list-item.component.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 export default class TransactionsListItem extends Component {
     render() {
         return(
-            <li className="list-group-item">
-                {this.props.item.type} ({this.props.item.ticker}) - {this.props.item.quantity} {this.props.item.quantity > 1 ? "Shares" : "Share"} @ {this.props.item.price}
-            </li>
+            <tr>
+                <td>{this.props.item.type}</td>
+                <td>{this.props.item.ticker}</td>
+                <td>{this.props.item.quantity} {this.props.item.quantity > 1 ? "Shares" : "Share"}</td>
+                <td>${this.props.item.price.toFixed(2)}</td>
+            </tr>
         );
     }
 }
