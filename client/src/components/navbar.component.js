2abf048467c95dd38a10dc850a9ab52e2cffb839
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-13 16:03:27 -0500

    Cleaned up navbar, aligned links to the left

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -8,11 +8,10 @@ export default class Navbar extends Component {
             <nav className="navbar navbar-expand navbar-dark bg-dark">
                 <Link to="/signin" className="navbar-brand">Stock Portfolio</Link>
                 <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav ml-auto">
                         <li className="navbar-item">
                             <Link to="/signin" className="nav-link">Signin</Link>
                         </li>
-                        <li className="navbar-item nav-link">|</li>
                         <li className="navbar-item">
                             <Link to="/register" className="nav-link">Register</Link>
                         </li>
