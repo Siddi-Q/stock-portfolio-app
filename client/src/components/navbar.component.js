baf0c9a597bb8dfa8627dc28b22b814463a0e052
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-14 15:14:50 -0500

    Updated public navbar to be more mobile friendly

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
-
-    render(){
+    render() {
         return(
-            <nav className="navbar navbar-expand navbar-dark bg-dark">
-                <Link to="/signin" className="navbar-brand">Stock Portfolio</Link>
-                <div className="collapse navbar-collapse">
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#publicNavbar" aria-controls="publicNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="publicNavbar">
+                    <Link to="/signin" className="navbar-brand">Stock Portfolio</Link>
                     <ul className="navbar-nav ml-auto">
                         <li className="navbar-item">
                             <Link to="/signin" className="nav-link">Signin</Link>
