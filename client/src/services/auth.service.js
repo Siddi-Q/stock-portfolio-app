15770e92565069329471c25a773048970312c95e
Saddique Shafi <saddiqueashafi@gmail.com>
2020-12-29 17:17:04 -0500

    Created auth.service.js; Added verify(), register(), signin(), and logout()

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export const verify = () => {
+    return axios.post('/verify', null, {
+        headers: {Authorization: sessionStorage.token}
+    });
+};
+
+export const register = async (name, email, password) => {
+    const userInfo = {
+        name,
+        email,
+        password
+    }
+
+    const res = await axios.post('/register', userInfo);
+    sessionStorage.setItem('token', res.data.authToken);
+};
+
+export const signin = async (email, password) => {
+    const userCredentials = {
+        email,
+        password
+    }
+    
+    const res = await axios.post('/signin', userCredentials);
+    sessionStorage.setItem('token', res.data.authToken);
+};
+
+export const logout = async () => {
+    await axios.post('/logout', null, {
+        headers: {Authorization: sessionStorage.token}
+    });
+    sessionStorage.removeItem("token");
+}
