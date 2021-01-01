782e09e7679161f8893e7d72015a8928f795e136
Saddique Shafi <saddiqueashafi@gmail.com>
2021-01-01 12:10:56 -0500

    Renamed functions in register component

--- client/src/components/auth/register.component.js
+++ client/src/components/auth/register.component.js
@@ -8,19 +8,19 @@ export default function Register(props) {
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
-    function onChangeName(event) {
+    function handleNameChange(event) {
         setName(event.target.value);
     }
 
-    function onChangeEmail(event) {
+    function handleEmailChange(event) {
         setEmail(event.target.value);
     }
 
-    function onChangePassword(event) {
+    function handlePasswordChange(event) {
         setPassword(event.target.value);
     }
 
-    function onSubmit(event) {
+    function handleSubmit(event) {
         event.preventDefault();
 
         register(name, email, password)
@@ -37,21 +37,21 @@ export default function Register(props) {
             <div className="col-lg-5 col-md-7 col-sm-10 col-12 align-self-end">
                 <div className="card card-container" style={{padding: "40px 40px", backgroundColor: "#f7f7f7"}}>
                     <h3 className="text-center" style={{padding: "0px 0px 10px 0px"}}>Register</h3>
-                    <form onSubmit={onSubmit}>
+                    <form onSubmit={handleSubmit}>
                         <div className="form-group">
                             <label className="sr-only">Name</label>
                             <input required type="text" className="form-control" placeholder="Name" autoFocus
-                                value={name} onChange={onChangeName} />
+                                value={name} onChange={handleNameChange} />
                         </div>
                         <div className="form-group">
                             <label className="sr-only">Email</label>
                             <input required type="email" className="form-control" placeholder="Email"
-                                value={email} onChange={onChangeEmail} />
+                                value={email} onChange={handleEmailChange} />
                         </div>
                         <div className="form-group">
                             <label className="sr-only">Password</label>
                             <input required type="password" className="form-control" placeholder="Password"
-                                value={password} onChange={onChangePassword} />
+                                value={password} onChange={handlePasswordChange} />
                         </div>
                         {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p> }
                         <div className="form-group">
