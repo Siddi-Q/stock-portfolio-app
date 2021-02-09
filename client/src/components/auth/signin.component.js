775d4e2246ab189ebe03414f47b941362b3e8036
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-09 16:40:44 -0500

    Updated signin component to use formik

--- client/src/components/auth/signin.component.js
+++ client/src/components/auth/signin.component.js
@@ -1,55 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
 
 import { signin } from '../../services/auth.service';
 
 function Signin(props) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-
-    const handleEmailChange = event => setEmail(event.target.value);
-
-    const handlePasswordChange = event => setPassword(event.target.value);
-
-    const handleSubmit = event => {
-        event.preventDefault();
-
-        signin(email, password)
-        .then(() => {
-            props.setIsAuth(true);
-        })
-        .catch((error) => {
-            setErrorMessage(error.response.data.message);
-        })
-    }
-
     return (
-        <div className="row justify-content-center">
-            <div className="col-lg-5 col-md-7 col-sm-10 col-12 align-self-end">
-                <div className="card card-container" style={{padding: "40px 40px", backgroundColor: "#f7f7f7"}}>
-                    <h3 className="text-center" style={{padding: "0px 0px 10px 0px"}}>Sign in</h3>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                                <label className="sr-only">Email</label>
-                                <input required type="email" className="form-control" placeholder="Email" autoFocus
-                                    value={email} onChange={handleEmailChange} />
+        <Formik
+            initialValues={{email: '', password: ''}}
+            validationSchema={Yup.object({
+                email: Yup.string().email('Invalid email address').required('Required'),
+                password: Yup.string().required('Required')
+            })}
+            onSubmit={(values, actions) => {
+                const { email, password } = values;
+                signin(email, password)
+                .then(() => {
+                    props.setIsAuth(true);
+                })
+                .catch((error) => {
+                    actions.setStatus(error.response.data.message);
+                    actions.setSubmitting(false);
+                });
+            }}
+        >
+            {formik => (
+                <div className="row justify-content-center">
+                    <div className="col-lg-5 col-md-7 col-sm-10 col-12 align-self-end">
+                        <div className="card card-container" style={{padding: "40px 40px", backgroundColor: "#f7f7f7"}}>
+                            <h3 className="text-center" style={{padding: "0px 0px 10px 0px"}}>Sign in</h3>
+                            <Form>
+                                <div className="form-group">
+                                    <label className="sr-only">Email</label>
+                                    <Field 
+                                        id="email" name="email" type="email" 
+                                        className="form-control" placeholder="Email"/>
+                                    <ErrorMessage component="div" name="email" style={{color: "red"}} />
+                                </div>
+                                <div className="form-group">
+                                    <label className="sr-only">Password</label>
+                                    <Field 
+                                        id="password" name="password" type="password" 
+                                        className="form-control" placeholder="Password"/>
+                                    <ErrorMessage component="div" name="password" style={{color: "red"}}/>
+                                </div>
+                                {formik.status && <div className="alert alert-danger" role="alert">{formik.status}</div> }
+                                <div className="form-group">
+                                    <input type="submit" value="Sign in" className="btn btn-primary btn-block" disabled={!Boolean(formik.values.email) || !Boolean(formik.values.password) || !formik.isValid || (formik.isSubmitting)} />
+                                </div>
+                            </Form>
+                            <Link to="/register">Don't have an account? Sign Up</Link>
                         </div>
-                        <div className="form-group">
-                                <label className="sr-only">Password</label>
-                                <input required type="password" className="form-control" placeholder="Password"
-                                    value={password} onChange={handlePasswordChange} />
-                        </div>
-                        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p> }
-                        <div className="form-group">
-                            <input type="submit" value="Sign in" className="btn btn-primary btn-block" />
-                        </div>
-                    </form>
-                    <Link to="/register">Don't have an account? Sign Up</Link>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </Formik>
     );
 }
 
