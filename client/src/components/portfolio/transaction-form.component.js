f8bd1732b0b31af3c413c61776bc526384850e54
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-20 18:39:33 -0500

    Updated TransactionForm component to use BuyForm component

--- client/src/components/portfolio/transaction-form.component.js
+++ client/src/components/portfolio/transaction-form.component.js
@@ -1,5 +1,6 @@
 import React  from 'react';
 import PropTypes from 'prop-types';
+import BuyForm from './buyForm.component';
 
 function TransactionForm(props) {
     const handleTickerChange = event => props.onTickerChange(event.target.value);
@@ -28,22 +29,12 @@ function TransactionForm(props) {
             <div className="tab-content" id="nav-tabContent">
                 <div className="tab-pane fade show active" id="nav-buy" role="tabpanel" aria-labelledby="nav-home-tab">
                     <br />
-                    <form onSubmit={handleBuySubmit}>
-                        <div className="form-group">
-                            <label className="sr-only">Ticker</label>
-                            <input required type="text" className="form-control" placeholder="Ticker" autoFocus
-                                value={props.ticker} onChange={handleTickerChange} />
-                        </div>
-                        <div className="form-group">
-                            <label className="sr-only">Quantity</label>
-                            <input required type="number" className="form-control" placeholder="Quantity" min="1"
-                                value={props.quantity} onChange={handleQuantityChange} />
-                        </div>
-                        {props.errorMessage && <p style={{ color: "red" }}>{props.errorMessage}</p> }
-                        <div className="form-group">
-                            <input type="submit" value="Buy" className="btn btn-primary btn-block" />
-                        </div>
-                    </form>
+                    <BuyForm 
+                        handleTickerChange={handleTickerChange} 
+                        handleQuantityChange={handleQuantityChange}
+                        handleBuySubmit={handleBuySubmit}
+                        {...props}
+                    />
                 </div>
                 <div className="tab-pane fade" id="nav-sell" role="tabpanel" aria-labelledby="nav-profile-tab">
                     <br />
