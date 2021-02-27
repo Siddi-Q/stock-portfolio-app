b4af6ede684d1a3811aa1f63883e8135054a540d
Saddique Shafi <saddiqueashafi@gmail.com>
2021-02-27 18:01:04 -0500

    Updated sellForm to use Formik

--- client/src/components/portfolio/sellForm.component.js
+++ client/src/components/portfolio/sellForm.component.js
@@ -1,25 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
 
 import { getPortfolio, sell } from '../../services/user.service';
 
 function SellForm(props) {
-    const [sellTicker, setSellTicker] = useState('');
-    const [sellQuantity, setSellQuantity] = useState('');
-
-    const handleSellTickerChange = event => setSellTicker(event.target.value);
-    const handleSellQuantityChange = event => setSellQuantity(event.target.value);
-
-    async function handleSellSubmit(event) {
-        event.preventDefault();
+    async function handleSellSubmit(values) {
         try {
+            const {sellTicker, sellQuantity} = values;
             await sell(sellTicker, sellQuantity);
 
             const res = await getPortfolio();
-    
             const {portfolioList, balance, totalPortfolioPrice} = res.data;
-            setSellTicker('');
-            setSellQuantity('');
+
             props.setPortfolioList(portfolioList);
             props.setBalance(balance);
             props.setTotalPortfolioPrice(totalPortfolioPrice);
@@ -30,22 +25,35 @@ function SellForm(props) {
     }
 
     return (
-        <form onSubmit={handleSellSubmit}>
-            <div className="form-group">
-                <label className="sr-only">Ticker</label>
-                <input required type="text" className="form-control" placeholder="Ticker" autoFocus
-                    value={sellTicker} onChange={handleSellTickerChange} />
-            </div>
-            <div className="form-group">
-                <label className="sr-only">Quantity</label>
-                <input required type="number" className="form-control" placeholder="Quantity" min="1"
-                    value={sellQuantity} onChange={handleSellQuantityChange} />
-            </div>
-            {props.errorMessage && <p style={{ color: "red" }}>{props.errorMessage}</p> }
-            <div className="form-group">
-                <input type="submit" value="Sell" className="btn btn-primary btn-block" />
-            </div>
-        </form>
+        <Formik
+            initialValues={{sellTicker: '', sellQuantity: ''}}
+            validationSchema={Yup.object({
+                sellTicker: Yup.string().required('Required'),
+                sellQuantity: Yup.number().required('Required')
+            })}
+            onSubmit={(values) => handleSellSubmit(values)}
+        >
+            {formik => (
+                <Form>
+                    <div className="form-group">
+                        <label className="sr-only">Ticker</label>
+                        <Field id="sellTicker" name="sellTicker" type="text" 
+                            className="form-control" placeholder="Ticker"/>
+                        <ErrorMessage component="div" name="sellTicker" style={{color: "red"}} />
+                    </div>
+                    <div className="form-group">
+                        <label className="sr-only">Quantity</label>
+                        <Field id="sellQuantity" name="sellQuantity" type="number" 
+                            className="form-control" placeholder="Quantity" min="1"/>
+                        <ErrorMessage component="div" name="sellQuantity" style={{color: "red"}} />
+                    </div>
+                    {props.errorMessage && <p style={{ color: "red" }}>{props.errorMessage}</p> }
+                    <div className="form-group">
+                        <input type="submit" value="Sell" className="btn btn-primary btn-block" />
+                    </div>
+                </Form>
+            )}
+        </Formik>
     );
 }
 
