ba786a6b32e1e97b00e6ffbe0110d3ef46504aed
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-10 18:14:39 -0500

    Added sorting (up/down) icons

--- client/src/components/transactions/transactions-list.component.js
+++ client/src/components/transactions/transactions-list.component.js
@@ -55,6 +55,14 @@ import LoadingSpinner from '../common/loading-spinner.component';
 
 import { getTransactions } from '../../services/user.service';
 
+const caretUpFill = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                        <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                    </svg>;
+
+const caretDownFill = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                        <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                      </svg>;
+
 export default function TransactionsList() {
     const [transactionsList, setTransactionsList] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -113,6 +121,10 @@ export default function TransactionsList() {
                                         {headerGroup.headers.map(column => (
                                             <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                                                 {column.render('Header')}
+                                                {' '}
+                                                <span>
+                                                    {column.isSorted ? (column.isSortedDesc ? caretDownFill : caretUpFill ) : "" }
+                                                </span>
                                             </th>
                                         ))}
                                     </tr>
