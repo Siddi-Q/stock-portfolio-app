effeba24b7938d33f3261bae6c4d8182d8b56b3c
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-17 16:46:27 -0400

    Updated porfolio-list-item component to use icons.js

--- client/src/components/portfolio/portfolio-list-item.component.js
+++ client/src/components/portfolio/portfolio-list-item.component.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {caretUpFill, caretDownFill, dash} from '../../icons/icons';
+
 function chooseStyle(performance) {
     const performanceColorObj = {"less": "red", "equal": "gray", "greater": "green"};
     return {color: performanceColorObj[performance]}
 }
 
 function chooseIcon(performance) {
-    const iconObj = {
-        "less":     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
-                        <path d="M7.247 11.14L2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-                    </svg>,
-        "equal":    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash" viewBox="0 0 16 16">
-                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
-                    </svg>,
-        "greater":  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill" viewBox="0 0 16 16">
-                        <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
-                    </svg>
-    };
-    return iconObj[performance]
+    const iconObj = {"less": caretDownFill, "equal": dash, "greater": caretUpFill};
+    return iconObj[performance];
 }
 
 function PortfolioListItem(props) {
