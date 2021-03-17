8fc8fe64636a626b49ca9975fc4cf7cdd1a537b3
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-17 16:45:48 -0400

    Moved dash icon to icons.js

--- client/src/icons/icons.js
+++ client/src/icons/icons.js
@@ -25,3 +25,7 @@ export const chevronDoubleRight = <svg xmlns="http://www.w3.org/2000/svg" width=
                             <path fillRule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
                             <path fillRule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
                            </svg>;
+
+export const dash = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash" viewBox="0 0 16 16">
+<path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+</svg>;
