f1c2a777a3489b4f19befe642c55441daa91c5e4
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-25 14:03:00 -0400

    Updated auth routes to start with /auth

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(authRouter);
+app.use('/auth', authRouter);
 app.use(userRouter);
 
 app.use(express.static(path.join(__dirname, 'client/build')));
