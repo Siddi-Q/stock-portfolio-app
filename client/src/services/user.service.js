cf3dc2b73c9882f11756e35a27513ebeb390732f
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-26 14:58:45 -0400

    Updated http user requests to use updated uri's

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const getPortfolio = () => {
-    return axios.get('/portfolio', {
+    return axios.get('/user/portfolio', {
         headers: {Authorization: 'Bearer ' + sessionStorage.token}
     });
 };
 
 export const getTransactions = () => {
-    return axios.get('/transactions',{
+    return axios.get('/user/transactions',{
         headers: {Authorization: 'Bearer ' + sessionStorage.token}
     });
 };
