a526bbf51dc33c129f2e21c1ef962dc50181f5a6
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-27 17:56:28 -0400

    Cleaned up portfolioListItem component; Added styling to link

--- client/src/components/portfolio/portfolio-list-item.component.js
+++ client/src/components/portfolio/portfolio-list-item.component.js
@@ -18,8 +18,7 @@ function PortfolioListItem(props) {
     return (
         <tr style={chooseStyle(props.item.performance)}>
             <td>{chooseIcon(props.item.performance)}</td>
-            {/* <td>{props.item.ticker}</td> */}
-            <td><Link to={`${props.item.ticker}/company`}>{props.item.ticker}</Link></td>
+            <td><Link to={`${props.item.ticker}/company`} style={chooseStyle(props.item.performance)}>{props.item.ticker}</Link></td>
             <td>{props.item.quantity} {props.item.quantity > 1 ? "Shares" : "Share"}</td>
             <td>${props.item.totalPrice.toFixed(2)}</td>
         </tr>
