8c7558a71c702784831b65b50d13bc1fb88e1894
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-27 18:19:35 -0400

    Updated to use useEffect() and getCompanyInfo()

--- client/src/components/company/company.component.js
+++ client/src/components/company/company.component.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
+import { getCompanyInfo } from '../../services/stock.service';
+
 export default function Company() {
+    const [companyInfo, setCompanyInfo] = useState();
     const { ticker } = useParams();
+
+    useEffect(() => {
+        getCompanyInfo(ticker)
+        .then(res => {
+            setCompanyInfo(res.data);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }, []);
     
     return (
-        <h1>Company - {ticker}</h1>
+        <>
+            <h1>Company - {ticker}</h1>
+            <p>{JSON.stringify(companyInfo, null, 2)}</p>
+        </>
     );
 }
