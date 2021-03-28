0490817f02a60d6d93d7526f83eac461e44e7b3f
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-28 14:10:33 -0400

    Updated returned JSX

--- client/src/components/company/company.component.js
+++ client/src/components/company/company.component.js
@@ -30,10 +30,34 @@ export default function Company() {
         );
     }
     else {
+        const { address, address2, CEO, city, companyName, country, description, employees, industry, phone, sector, state, symbol, tags, website, zip } = companyInfo;
+
         return (
             <>
-                <h1>Company - {ticker}</h1>
-                <p>{JSON.stringify(companyInfo, null, 2)}</p>
+                <div className="row">
+                    <div className="col-sm-12 col-md-6 col-lg-8">
+                        <h1>{companyName}</h1>
+                        <p>{symbol}</p>
+                    </div>
+                    <div className="col-sm-12 col-md-6 col-lg-4">
+                        <p>{website}</p>
+                        <p>{phone}</p>
+                        <p>{address}</p>
+                        <p>{address2}</p>
+                        <p>{city}, {state}, {country}</p>
+                        <p>{zip}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <p>{CEO}</p>
+                        <p>{industry}</p>
+                        <p>{sector}</p>
+                        {tags.map((tag, idx) => <p key={idx}>{tag}</p>)}
+                        <p>{employees}</p>
+                        <p>{description}</p>
+                    </div>
+                </div>
             </>
         );
     }
