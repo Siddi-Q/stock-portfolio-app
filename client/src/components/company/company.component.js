d4dd136fdcf7b726dbcf884f3152ef8419465fe3
Saddique Shafi <saddiqueashafi@gmail.com>
2021-03-28 12:43:39 -0400

    Added loading spinner to Company component

--- client/src/components/company/company.component.js
+++ client/src/components/company/company.component.js
@@ -1,26 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
+import LoadingSpinner from '../common/loading-spinner.component';
+
 import { getCompanyInfo } from '../../services/stock.service';
 
 export default function Company() {
     const [companyInfo, setCompanyInfo] = useState();
+    const [loading, setLoading] = useState(true);
+
     const { ticker } = useParams();
 
     useEffect(() => {
         getCompanyInfo(ticker)
         .then(res => {
             setCompanyInfo(res.data);
+            setLoading(false);
         })
         .catch(error => {
             console.error(error);
         })
-    }, []);
-    
-    return (
-        <>
-            <h1>Company - {ticker}</h1>
-            <p>{JSON.stringify(companyInfo, null, 2)}</p>
-        </>
-    );
+    }, [ticker]);
+
+    if(loading) {
+        return (
+            <div className="row justify-content-center">
+                <LoadingSpinner/>            
+            </div>
+        );
+    }
+    else {
+        return (
+            <>
+                <h1>Company - {ticker}</h1>
+                <p>{JSON.stringify(companyInfo, null, 2)}</p>
+            </>
+        );
+    }
 }
