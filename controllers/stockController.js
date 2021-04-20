7659323b98c834aa869a711780d34d7fe33fc27f
Saddique Shafi <saddiqueashafi@gmail.com>
2021-04-20 17:26:15 -0400

    Moved sellStock() to stockController.js

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -45,6 +45,51 @@ const buyStock = async (req, res) => {
   }
 }
 
+const sellStock = async (req, res) => {
+  let {ticker, quantity} = req.body;
+  ticker = ticker.toUpperCase();
+  quantity = Number(quantity);
+
+  if (process.env.NODE_ENV === 'production') {
+    var apiUrl = `https://cloud.iexapis.com/stable/stock/${ticker}/quote?token=${process.env.iexToken}`;
+  } else {
+    var apiUrl = `https://sandbox.iexapis.com/stable/stock/${ticker}/quote?token=${process.env.iexSandboxToken}`;
+  }
+
+  try {
+    if (!Number.isInteger(quantity) || quantity < 0 ) {
+      throw new Error('Quantity is not a whole number!');
+    }
+
+    let {balance, portfolio, transactions} = req.user;
+    const {data} = await axios.get(apiUrl);
+    const {latestPrice} = data;
+    const totalPrice = latestPrice * quantity;
+
+    // Check if the user has enoough of a certain stock
+    const stockIndex = portfolio.findIndex(stock => stock.ticker === ticker && stock.quantity >= quantity);
+    if (stockIndex === -1) {
+      throw new Error('Cannot sell this stock!');
+    }
+
+    balance += totalPrice;
+    transactions.push({type: 'SELL', ticker, quantity, price: latestPrice, date: new Date()});
+
+    const stock = portfolio[stockIndex];
+    if (stock.quantity === quantity) {
+      portfolio.splice(stockIndex, 1);
+    } else {
+      stock.quantity -= quantity;
+    }
+
+    await User.findByIdAndUpdate(req.user.id, {transactions, balance, portfolio});
+    res.status(201).send('Good!');
+  } catch(error) {
+    res.status(400).send({message: error.message});
+  }
+}
+
 module.exports = {
     buyStock,
+    sellStock,
 }
