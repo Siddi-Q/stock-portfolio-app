ef80a0b24f9257f475829018755f709fe623581a
Saddique Shafi <saddiqueashafi@gmail.com>
2021-04-22 16:42:04 -0400

    Cleaned up stock.js and auth.js

--- routers/auth.js
+++ routers/auth.js
@@ -1,15 +1,13 @@
 const express = require('express');
+
 const auth = require('../middleware/auth');
 const authController = require('../controllers/authController');
 
 const authRouter = new express.Router();
 
 authRouter.post('/register', authController.registerNewUser);
-
 authRouter.post('/signin', authController.signinUser);
-
 authRouter.post('/logout', auth, authController.logoutUser);
-
 authRouter.post('/verify', auth, authController.verifyUser);
 
 module.exports = authRouter;
--- routers/stock.js
+++ routers/stock.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const axios = require('axios');
-const User = require('../models/user');
-const auth = require('../middleware/auth');
 
+const auth = require('../middleware/auth');
 const stockController = require('../controllers/stockController');
 
 const stockRouter = new express.Router();
 
 stockRouter.post('/buy', auth, stockController.buyStock);
-
 stockRouter.post('/sell', auth, stockController.sellStock);
-
 stockRouter.get('/:ticker/company', auth, stockController.getCompanyInfo);
 
 module.exports = stockRouter;
