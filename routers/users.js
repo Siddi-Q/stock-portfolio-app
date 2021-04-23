0c071395a0bb13a6dd39a63cbe592e757cc40572
Saddique Shafi <saddiqueashafi@gmail.com>
2021-04-23 14:44:26 -0400

    Moved getPortfolio() to usersController.js

--- routers/users.js
+++ routers/users.js
@@ -2,46 +2,11 @@ const express = require('express');
 const axios = require('axios');
 const auth = require('../middleware/auth');
 
+const usersController = require('../controllers/usersController');
+
 const userRouter = new express.Router();
 
-userRouter.get('/portfolio', auth, async (req, res) => {
-  try {
-    const {portfolio: portfolioList, balance} = req.user;
-    let totalPortfolioPrice = 0;
-
-    const symbols = portfolioList.map(portfolioItem => portfolioItem.ticker.toUpperCase());
-
-    if (symbols.length > 0) {
-      const symbolsString = symbols.join();
-
-      if (process.env.NODE_ENV === 'production') {
-        var apiUrl = `https://cloud.iexapis.com/stable/stock/market/batch?symbols=${symbolsString}&types=quote&token=${process.env.iexToken}`;
-      } else {
-        var apiUrl = `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${symbolsString}&types=quote&token=${process.env.iexSandboxToken}`;
-      }
-
-      const {data} = await axios.get(apiUrl);
-
-      portfolioList.forEach(portfolioItem => {
-        const {latestPrice, open: openPrice} = data[portfolioItem.ticker].quote;
-        portfolioItem.totalPrice = latestPrice * portfolioItem.quantity;
-        totalPortfolioPrice += portfolioItem.totalPrice;
-
-        if (latestPrice < openPrice) {
-          portfolioItem.performance = 'less';
-        } else if (latestPrice > openPrice) {
-          portfolioItem.performance = 'greater';
-        } else {
-          portfolioItem.performance = 'equal';
-        }
-      });
-    }
-
-    res.send({portfolioList, balance, totalPortfolioPrice});
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+userRouter.get('/portfolio', auth, usersController.getPortfolio);
 
 userRouter.get('/transactions', auth, async (req, res) => {
   try {
