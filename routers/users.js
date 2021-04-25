642a0fcbf7af17f0bd570bfc35f8fc547c763680
Saddique Shafi <saddiqueashafi@gmail.com>
2021-04-25 13:10:22 -0400

    Cleaned up users.js

--- routers/users.js
+++ routers/users.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const axios = require('axios');
-const auth = require('../middleware/auth');
 
+const auth = require('../middleware/auth');
 const usersController = require('../controllers/usersController');
 
 const userRouter = new express.Router();
 
 userRouter.get('/portfolio', auth, usersController.getPortfolio);
-
 userRouter.get('/transactions', auth, usersController.getTransactions);
 
 module.exports = userRouter;
