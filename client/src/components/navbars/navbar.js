dd9982b75b3806a1b21049f836963a98cfed0f3d
Saddique Shafi <saddiqueashafi@gmail.com>
2021-04-27 15:18:31 -0400

    Refactored/Cleaned up navbar.js

--- client/src/components/navbars/navbar.js
+++ client/src/components/navbars/navbar.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    return(
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#publicNavbar" aria-controls="publicNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="publicNavbar">
-                <Link to="/signin" className="navbar-brand">Stock Portfolio</Link>
-                <ul className="navbar-nav ml-auto">
-                    <li className="navbar-item">
-                        <Link to="/signin" className="nav-link">Signin</Link>
-                    </li>
-                    <li className="navbar-item">
-                        <Link to="/register" className="nav-link">Register</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#publicNavbar" aria-controls="publicNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="publicNavbar">
+        <Link to="/signin" className="navbar-brand">Stock Portfolio</Link>
+        <ul className="navbar-nav ml-auto">
+          <li className="navbar-item">
+            <Link to="/signin" className="nav-link">Signin</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/register" className="nav-link">Register</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
