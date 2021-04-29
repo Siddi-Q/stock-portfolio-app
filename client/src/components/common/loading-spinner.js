e27322bed166d06aeb3e9979dd778f1a9503edca
Saddique Shafi <saddiqueashafi@gmail.com>
2021-04-29 16:05:43 -0400

    Refactored/Cleaned up loading-spinner.js

--- client/src/components/common/loading-spinner.js
+++ client/src/components/common/loading-spinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function LoadingSpinner() {
-    return  (
-        <div className="spinner-border" style={{width: "10rem", height: "10rem"}} role="status">
-            <span className="sr-only">Loading...</span>
-        </div>
-    );
+  return (
+    <div className="spinner-border" style={{width: "10rem", height: "10rem"}} role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
 }
