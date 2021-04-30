a623f045ba346051ed306c49fb915a2be772d5c6
Saddique Shafi <saddiqueashafi@gmail.com>
2021-04-30 15:03:55 -0400

    Refactored/Cleaned up logout.js

--- client/src/components/auth/logout.js
+++ client/src/components/auth/logout.js
@@ -5,26 +5,24 @@ import LoadingSpinner from '../common/loading-spinner';
 
 import { logout } from '../../services/auth';
 
-function Logout(props) {
-    useEffect(() => {
-        logout()
-        .then(() => {
-            props.setIsAuth(false);
-        })
-        .catch((error) => {
-            console.error(error);
-        })
+export default function Logout(props) {
+  useEffect(() => {
+    logout()
+    .then(() => {
+      props.setIsAuth(false);
+    })
+    .catch((error) => {
+      console.error(error);
     });
+  });
 
-    return (
-        <div className="row justify-content-center">
-            <LoadingSpinner />
-        </div>
-    );
+  return (
+    <div className="row justify-content-center">
+      <LoadingSpinner />
+    </div>
+  );
 }
 
 Logout.propTypes = {
-    setIsAuth: PropTypes.func.isRequired
+  setIsAuth: PropTypes.func.isRequired,
 }
-
-export default Logout;
