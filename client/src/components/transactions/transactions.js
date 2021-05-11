cb9cd52c5cb815ced6ed5df20eefb1d3d57b5fb1
Saddique Shafi <saddiqueashafi@gmail.com>
2021-05-11 12:56:36 -0400

    Refactored/Cleaned up transactions.js

--- client/src/components/transactions/transactions.js
+++ client/src/components/transactions/transactions.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useFilters, useGlobalFilter, usePagination, useSortBy, useTable } from 'react-table';
-import { format, parseISO } from "date-fns";
+import { format, parseISO } from 'date-fns';
 
 import GlobalFilter from './global-filter';
 import TickerFilter from './ticker-filter';
@@ -10,164 +10,165 @@ import LoadingSpinner from '../common/loading-spinner';
 import { getTransactions } from '../../services/user';
 
 import {caretUpFill, caretDownFill, chevronLeft, chevronDoubleLeft, chevronRight, chevronDoubleRight} from '../../icons/icons';
-import "../../styles/styles.css";
+import '../../styles/styles.css';
 
 export default function Transactions() {
-    const [transactionsList, setTransactionsList] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [transactionsList, setTransactionsList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  const columns = useMemo(
+    () => [
+      {Header: 'Type', accessor: 'type'},
+      {Header: 'Ticker', accessor: 'ticker', Cell: ({value}) => value ? <Link to={`${value}/company`}>{value}</Link> : ''},
+      {Header: 'Quantity', accessor: 'quantity', Cell: ({value}) => value ? value + ' Share' + (value > 1 ? 's' : '') : ''},
+      {Header: 'Price', accessor: 'price', Cell: ({value}) => value ? '$' + value.toFixed(2) : ''},
+      {Header: 'Date', accessor: 'date', Cell: ({value}) => value ? format(new Date(parseISO(value)), 'MMM d, yyyy') : ''},
+    ], []
+  );
 
-    const columns = useMemo(
-        () => [
-            {Header: 'Type', accessor: 'type'},
-            {Header: 'Ticker', accessor: 'ticker', Cell: ({value}) => value ? <Link to={`${value}/company`}>{value}</Link> : ""},
-            {Header: 'Quantity', accessor: 'quantity', Cell: ({value}) => value ? value + " Share" + (value > 1 ? "s" : "") : ""},
-            {Header: 'Price', accessor: 'price', Cell: ({value}) => value ? "$" + value.toFixed(2) : ""},
-            {Header: 'Date', accessor: 'date', Cell: ({value}) => value ? format(new Date(parseISO(value)), "MMM d, yyyy") : "" }
-        ], []);
+  useEffect(() => {
+    getTransactions()
+    .then(res => {
+      setTransactionsList(res.data);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }, []);
 
-    useEffect(() => {
-        getTransactions()
-        .then(res => {
-            setTransactionsList(res.data);
-            setLoading(false);
-        })
-        .catch(error => {
-            console.error(error);
-        })
-    }, []);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    pageCount,
+    canNextPage,
+    canPreviousPage,
+    gotoPage,
+    nextPage,
+    previousPage,
+    prepareRow,
+    state,
+    setFilter,
+    setAllFilters,
+    setGlobalFilter,
+    preFilteredRows
+  } = useTable({columns, data: transactionsList}, useFilters, useGlobalFilter, useSortBy, usePagination);
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        page,
-        pageCount,
-        canNextPage,
-        canPreviousPage,
-        gotoPage,
-        nextPage,
-        previousPage,
-        prepareRow,
-        state,
-        setFilter,
-        setAllFilters,
-        setGlobalFilter,
-        preFilteredRows
-    } = useTable({columns, data: transactionsList}, useFilters, useGlobalFilter, useSortBy, usePagination);
+  const { globalFilter } = state;
 
-    const { globalFilter } = state;
-
-    if(loading) {
-        return (
-            <div className="row justify-content-center">
-                <LoadingSpinner />
-            </div>
-        );
-    }
-    else {
-        return transactionsList.length === 0 ? 
-        (
-            <>
-                <h1 className="mb-3">Transactions <span className="badge badge-dark">{transactionsList.length}</span></h1>
-                <h3>You have no transactions!</h3>
-            </>
-        )
-        : 
-        (
-            <>
-                <h1 className="mb-3">Transactions <span className="badge badge-dark">{transactionsList.length}</span></h1>
-                <div className="row">
-                    <div className="col-12 col-md-8 mb-3">
-                        <form className="form-inline justify-content-end mb-2">
-                            <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter}  />
-                        </form>
-                        <table {...getTableProps()} className="table table-hover">
-                            <thead>
-                                {headerGroups.map(headerGroup => (
-                                    <tr {...headerGroup.getHeaderGroupProps()}>
-                                        {headerGroup.headers.map(column => (
-                                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                                {column.render('Header')}
-                                                {' '}
-                                                <span>
-                                                    {column.isSorted ? (column.isSortedDesc ? caretDownFill : caretUpFill ) : "" }
-                                                </span>
-                                            </th>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </thead>
-                            <tbody {...getTableBodyProps()}>
-                                {page.map(row => {
-                                    prepareRow(row);
-                                    return (
-                                        <tr {...row.getRowProps()}>
-                                            {row.cells.map(cell => {
-                                                return (
-                                                    <td {...cell.getCellProps()}>
-                                                        {cell.render('Cell')}
-                                                    </td>
-                                                );
-                                            })}
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                        <div className="btn-toolbar justify-content-center" role="toolbar">
-                            <div className="btn-group mr-2" role="group">
-                                <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{chevronDoubleLeft}</button>
-                            </div>
-                            <div className="btn-group mr-2" role="group">
-                                <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => previousPage()} disabled={!canPreviousPage}>{chevronLeft}</button>
-                            </div>
-                            <div className="btn-group mr-2" role="group">
-                                <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => nextPage()} disabled={!canNextPage}>{chevronRight}</button>
-                            </div>
-                            <div className="btn-group" role="group">
-                                <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{chevronDoubleRight}</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-4">
-                        <form>
-                            <div className="form-group row">
-                                <label className="col-4 col-form-label"><strong>Type</strong></label>
-                                <div className="col-8">
-                                    <select id="type-filter" className="custom-select" onChange={e => setFilter("type", e.target.value)}>
-                                        <option value="">ALL</option>
-                                        <option value="BUY">BUY</option>
-                                        <option value="SELL">SELL</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className="form-group row">
-                                <label className="col-4 col-form-label"><strong>Ticker</strong></label>
-                                <div className="col-8">
-                                    <TickerFilter preFilteredRows={preFilteredRows} setFilter={setFilter} />
-                                </div>
-                            </div>
-                            <div className="form-group row">
-                                <label className="col-4 col-form-label"><strong>Quantity</strong></label>
-                                <div className="col-8">
-                                    <input id="quantity-filter" type="number" min="1" className="form-control"
-                                    onChange={e => setFilter("quantity", Number(e.target.value))} />
-                                </div>
-                            </div>
-                            <div className="form-group row justify-content-center">
-                                <button type="button" className="btn btn-light border"
-                                onClick={() => {
-                                    setAllFilters([]);
-                                    document.getElementById("quantity-filter").value = null;
-                                    document.getElementById("type-filter").value = "";
-                                    document.getElementById("ticker-filter").value = "";
-                                }}>Reset</button>
-                            </div>
-                        </form>
-                    </div>
+  if(loading) {
+    return (
+      <div className="row justify-content-center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  else {
+    return transactionsList.length === 0 ?
+      (
+        <>
+          <h1 className="mb-3">Transactions <span className="badge badge-dark">{transactionsList.length}</span></h1>
+          <h3>You have no transactions!</h3>
+        </>
+      )
+      :
+      (
+        <>
+          <h1 className="mb-3">Transactions <span className="badge badge-dark">{transactionsList.length}</span></h1>
+          <div className="row">
+            <div className="col-12 col-md-8 mb-3">
+              <form className="form-inline justify-content-end mb-2">
+                <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+              </form>
+              <table {...getTableProps()} className="table table-hover">
+                <thead>
+                  {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map(column => (
+                        <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                          {column.render('Header')}
+                          {' '}
+                          <span>
+                            {column.isSorted ? (column.isSortedDesc ? caretDownFill : caretUpFill ) : ''}
+                          </span>
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                  {page.map(row => {
+                    prepareRow(row);
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map(cell => {
+                          return (
+                            <td {...cell.getCellProps()}>
+                              {cell.render('Cell')}
+                            </td>
+                          );
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              <div className="btn-toolbar justify-content-center" role="toolbar">
+                <div className="btn-group mr-2" role="group">
+                  <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{chevronDoubleLeft}</button>
                 </div>
-                <br />
-            </>
-        );
-    }
+                <div className="btn-group mr-2" role="group">
+                  <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => previousPage()} disabled={!canPreviousPage}>{chevronLeft}</button>
+                </div>
+                <div className="btn-group mr-2" role="group">
+                  <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => nextPage()} disabled={!canNextPage}>{chevronRight}</button>
+                </div>
+                <div className="btn-group" role="group">
+                  <button type="button" className="btn btn-outline-primary btn-circle d-flex justify-content-center align-items-center" onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{chevronDoubleRight}</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-md-4">
+              <form>
+                <div className="form-group row">
+                  <label className="col-4 col-form-label"><strong>Type</strong></label>
+                  <div className="col-8">
+                    <select id="type-filter" className="custom-select" onChange={e => setFilter('type', e.target.value)}>
+                      <option value="">ALL</option>
+                      <option value="BUY">BUY</option>
+                      <option value="SELL">SELL</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-4 col-form-label"><strong>Ticker</strong></label>
+                  <div className="col-8">
+                    <TickerFilter preFilteredRows={preFilteredRows} setFilter={setFilter} />
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-4 col-form-label"><strong>Quantity</strong></label>
+                  <div className="col-8">
+                    <input id="quantity-filter" type="number" min="1" className="form-control"
+                    onChange={e => setFilter("quantity", Number(e.target.value))} />
+                  </div>
+                </div>
+                <div className="form-group row justify-content-center">
+                  <button type="button" className="btn btn-light border"
+                  onClick={() => {
+                    setAllFilters([]);
+                    document.getElementById("quantity-filter").value = null;
+                    document.getElementById("type-filter").value = "";
+                    document.getElementById("ticker-filter").value = "";
+                  }}>Reset</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <br />
+        </>
+      );
+  }
 }
