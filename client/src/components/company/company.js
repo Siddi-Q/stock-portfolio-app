e21260d3f8bd51869d2931c6cce17a8aefdfd47a
Saddique Shafi <saddiqueashafi@gmail.com>
2021-05-12 14:52:21 -0400

    Refactored/Cleaned up company.js

--- client/src/components/company/company.js
+++ client/src/components/company/company.js
@@ -8,109 +8,107 @@ import { getCompanyInfo } from '../../services/stock';
 import { geoAlt, link45Deg, telephone } from '../../icons/icons';
 
 export default function Company() {
-    const [companyInfo, setCompanyInfo] = useState({});
-    const [loading, setLoading] = useState(true);
+  const [companyInfo, setCompanyInfo] = useState({});
+  const [loading, setLoading] = useState(true);
 
-    const { ticker } = useParams();
+  const { ticker } = useParams();
 
-    useEffect(() => {
-        getCompanyInfo(ticker)
-        .then(res => {
-            setCompanyInfo(res.data);
-            setLoading(false);
-        })
-        .catch(error => {
-            console.error(error);
-        })
-    }, [ticker]);
+  useEffect(() => {
+    getCompanyInfo(ticker)
+    .then(res => {
+      setCompanyInfo(res.data);
+      setLoading(false);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }, [ticker]);
 
-    if(loading) {
-        return (
-            <div className="row justify-content-center">
-                <LoadingSpinner/>            
-            </div>
-        );
-    }
-    else {
-        const { CEO, city, companyName, country, description, employees, industry, phone, sector, state, tags, website } = companyInfo;
+  if(loading) {
+    return (
+      <div className="row justify-content-center">
+        <LoadingSpinner />
+      </div>
+    );
+  } else {
+    const { CEO, city, companyName, country, description, employees, industry, phone, sector, state, tags, website } = companyInfo;
 
-        return (
+    return (
+      <>
+        <div className="row justify-content-center mb-2">
+          <img className="border" src={`https://storage.googleapis.com/iex/api/logos/${ticker}.png`} alt="Logo" />
+        </div>
+        <div className="row mb-2">
+          <div className="col-sm-12 col-md-6 col-lg-8">
+            <h1>{companyName}</h1>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-4">
+            {website !== "0" &&
             <>
-                <div className="row justify-content-center mb-2">
-                    <img className="border" src={`https://storage.googleapis.com/iex/api/logos/${ticker}.png`} alt="Logo"/>
-                </div>
-                <div className="row mb-2">
-                    <div className="col-sm-12 col-md-6 col-lg-8">
-                        <h1>{companyName}</h1>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-4">
-                        {website !== "0" &&
-                        <>
-                            {link45Deg}{' '}
-                            <a href={website} rel="noopener noreferrer" target="_blank">{website}</a>
-                            <br />
-                        </>
-                        }
-                        {phone !== "0" &&
-                        <>
-                            {telephone}{' '}
-                            <span>{phone}</span>
-                            <br />
-                        </>
-                        }
-                        {city !== "0" && state !== "0" && country !== "0" &&
-                        <>
-                            {geoAlt}{' '}
-                            <span>{city}, {state}, {country}</span>
-                            <br />
-                        </>
-                        }
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        {CEO !== "0" &&
-                        <>
-                            <span><strong>CEO</strong> {CEO}</span>
-                            <br />
-                        </>
-                        }
-                        {industry !== "0" &&
-                        <>
-                            <span><strong>Industry</strong> {industry}</span>
-                            <br />
-                        </>
-                        }
-                        {sector !== "0" &&
-                        <>
-                            <span><strong>Sector</strong> {sector}</span>
-                            <br />
-                        </>
-                        }
-                        {tags && 
-                        <>
-                            <span><strong>Tags </strong></span>
-                            {tags.map((tag, idx) => <span key={idx} className="badge badge-info text-wrap mr-1">{tag}</span>)}
-                            <br />
-                        </>
-                        }
-                        {employees &&
-                        <>
-                            <span><strong># of employees</strong> {employees}</span>
-                            <br />
-                        </>
-                        }
-                        
-                        <br />
-                        {description !== "0" && 
-                        <>
-                            <span><strong>Description</strong></span>
-                            <p>{description}</p>
-                        </>
-                        }
-                    </div>
-                </div>
+              {link45Deg}{' '}
+              <a href={website} rel="noopener noreferrer" target="_blank">{website}</a>
+              <br />
             </>
-        );
-    }
+            }
+            {phone !== '0' &&
+            <>
+              {telephone}{' '}
+              <span>{phone}</span>
+              <br />
+            </>
+            }
+            {city !== '0' && state !== '0' && country !== '0' &&
+            <>
+              {geoAlt}{' '}
+              <span>{city}, {state}, {country}</span>
+              <br />
+            </>
+            }
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            {CEO !== '0' &&
+            <>
+              <span><strong>CEO</strong> {CEO}</span>
+              <br />
+            </>
+            }
+            {industry !== '0' &&
+            <>
+              <span><strong>Industry</strong> {industry}</span>
+              <br />
+            </>
+            }
+            {sector !== '0' &&
+            <>
+              <span><strong>Sector</strong> {sector}</span>
+              <br />
+            </>
+            }
+            {tags && 
+            <>
+              <span><strong>Tags </strong></span>
+              {tags.map((tag, idx) => <span key={idx} className="badge badge-info text-wrap mr-1">{tag}</span>)}
+              <br />
+            </>
+            }
+            {employees &&
+            <>
+              <span><strong># of employees</strong> {employees}</span>
+              <br />
+            </>
+            }
+            <br />
+            {description !== '0' && 
+            <>
+              <span><strong>Description</strong></span>
+              <p>{description}</p>
+            </>
+            }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
