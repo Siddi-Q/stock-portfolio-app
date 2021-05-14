6e2229d4d92bfa33f9292a3a0ab85ead3290a066
Saddique Shafi <saddiqueashafi@gmail.com>
2021-05-14 16:13:57 -0400

    Refactored/Cleaned up auth.js

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,35 +1,36 @@
 import axios from 'axios';
 
 export const verify = () => {
-    return axios.post('/auth/verify', null, {
-        headers: {Authorization: 'Bearer ' + sessionStorage.token}
-    });
-};
+  return axios.post('/auth/verify', null, {
+    headers: {Authorization: 'Bearer ' + sessionStorage.token}
+  });
+}
 
 export const register = async (name, email, password) => {
-    const userInfo = {
-        name,
-        email,
-        password
-    }
+  const userInfo = {
+    name,
+    email,
+    password
+  }
 
-    const res = await axios.post('/auth/register', userInfo);
-    sessionStorage.setItem('token', res.data.authToken);
-};
+  const res = await axios.post('/auth/register', userInfo);
+  sessionStorage.setItem('token', res.data.authToken);
+}
 
 export const signin = async (email, password) => {
-    const userCredentials = {
-        email,
-        password
-    }
-    
-    const res = await axios.post('/auth/signin', userCredentials);
-    sessionStorage.setItem('token', res.data.authToken);
-};
+  const userCredentials = {
+    email,
+    password
+  }
+
+  const res = await axios.post('/auth/signin', userCredentials);
+  sessionStorage.setItem('token', res.data.authToken);
+}
 
 export const logout = async () => {
-    await axios.post('/auth/logout', null, {
-        headers: {Authorization: 'Bearer ' + sessionStorage.token}
-    });
-    sessionStorage.removeItem("token");
+  await axios.post('/auth/logout', null, {
+    headers: {Authorization: 'Bearer ' + sessionStorage.token}
+  });
+
+  sessionStorage.removeItem('token');
 }
