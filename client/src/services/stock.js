87f931fd7d739df1f0d54bbb00b82cf914ccbc5f
Saddique Shafi <saddiqueashafi@gmail.com>
2021-05-15 15:50:18 -0400

    Refactored/Cleaned up stock.js

--- client/src/services/stock.js
+++ client/src/services/stock.js
@@ -1,29 +1,29 @@
 import axios from 'axios';
 
 export const buy = async (ticker, quantity) => {
-    const newItem = {
-        ticker,
-        quantity
-    };
+  const newItem = {
+    ticker,
+    quantity
+  };
 
-    await axios.post('/stock/buy', newItem, {
-        headers: {Authorization: 'Bearer ' + sessionStorage.token}
-    });
+  await axios.post('/stock/buy', newItem, {
+    headers: {Authorization: 'Bearer ' + sessionStorage.token}
+  });
 }
 
 export const sell = async (ticker, quantity) => {
-    const newItem = {
-        ticker,
-        quantity
-    };
-    
-    await axios.post('/stock/sell', newItem, {
-        headers: {Authorization: 'Bearer ' + sessionStorage.token}
-    });
+  const newItem = {
+    ticker,
+    quantity
+  };
+
+  await axios.post('/stock/sell', newItem, {
+    headers: {Authorization: 'Bearer ' + sessionStorage.token}
+  });
 }
 
 export const getCompanyInfo = async (ticker) => {
-    return await axios.get(`/stock/${ticker}/company`, {
-        headers: {Authorization: 'Bearer ' + sessionStorage.token}
-    });
+  return await axios.get(`/stock/${ticker}/company`, {
+    headers: {Authorization: 'Bearer ' + sessionStorage.token}
+  });
 }
